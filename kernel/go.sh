d11c153da3272dace08b20998ec640e0fbf1484a
simon-falinux <simon@falinux.com>
2014-10-01 02:13:35 -0700

    [simon@falinux.com] re-organized directories

--- kernel/go.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./make-imx6.sh $1
-
-
-cp /tftpboot/uImage.imx6 /tftpboot/uImage.imx6-3.7
-cp imx6q-sabrelite.dtb /tftpboot/
-
-#scp /tftpboot/uImage.imx6 imx6q-sabrelite.dtb boggle70@192.168.10.190:/staff/tftpboot/
