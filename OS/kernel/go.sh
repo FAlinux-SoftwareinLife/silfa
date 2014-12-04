d2e38260805f4d1eb6dd276cf30c13b898dd3a43
Sangmin <simon@falinux.com>
2014-12-04 16:36:16 +0900

    [simon@falinux.com] fix go.sh scripts for ARM server

--- OS/kernel/go.sh
+++ OS/kernel/go.sh
@@ -2,8 +2,7 @@
 
 ./make-imx6.sh $1
 
+# cp output binaries to tftp folder
+#cp /tftpboot/uImage.imx6 /tftpboot/uImage.imx6-3.7
+#cp imx6q-sabrelite.dtb /tftpboot/
 
-cp /tftpboot/uImage.imx6 /tftpboot/uImage.imx6-3.7
-cp imx6q-sabrelite.dtb /tftpboot/
-
-#scp /tftpboot/uImage.imx6 imx6q-sabrelite.dtb boggle70@192.168.10.190:/staff/tftpboot/
